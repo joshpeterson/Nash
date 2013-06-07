afab15ef083d31dc8a6366bdc510c7480d87fecb
Joshua Peterson <petersonjm1@gmail.com>
2013-06-07 07:26:28 -0400

    Add more tests for the MpiMapReduce implementation
    * Verify the correct count of elements is passed to MPI_Send
    * Verify the correct integer version of the start iterator for the partition is passed to MPI_Send
    * Verify the correct integer version of the end iterator for the partition is passed to MPI_Send

--- MpiMapReduce.h
+++ MpiMapReduce.h
@@ -14,22 +14,25 @@ public:
 	MpiMapReduce(const MpiAdapterInterface& mpi_interface, IteratorType begin, IteratorType end,
 			     std::function<std::vector<std::pair<IteratorType, IteratorType>>(IteratorType, IteratorType, int)> partitioning_method,
 				 int number_of_threads) :
-	mpi_interface_(mpi_interface), begin_(begin), end_(end), partitioning_method_(partitioning_method), number_of_threads_(number_of_threads)
+	mpi_interface_(mpi_interface), begin_(begin), end_(end), partition_(partitioning_method), number_of_threads_(number_of_threads)
 	{
 	}
 
 	void map()
 	{
-		auto partitions = partitioning_method_(begin_, end_, number_of_threads_);
+		auto partitions = partition_(begin_, end_, number_of_threads_);
 		for (auto partition : partitions)
-			mpi_interface_.MpiSend(NULL, 0, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		{
+			int iterator_values[] = {convert_to_integer_(partition.first()), convert_to_integer_(partition.second())};
+			mpi_interface_.MpiSend(&iterator_values, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		}
 
 	}
 private:
 	const MpiAdapterInterface& mpi_interface_;
 	IteratorType begin_;
 	IteratorType end_;
-	std::function<std::vector<std::pair<IteratorType, IteratorType>>(IteratorType, IteratorType, int)> partitioning_method_;
+	std::function<std::vector<std::pair<IteratorType, IteratorType>>(IteratorType, IteratorType, int)> partition_;
 	int number_of_threads_;
 };
 
--- Test/MpiMapReduceTests.h
+++ Test/MpiMapReduceTests.h
@@ -11,6 +11,7 @@
 
 std::vector<std::pair<std::vector<int>::iterator, std::vector<int>::iterator>> one_partition(std::vector<int>::iterator, std::vector<int>::iterator, int);
 std::vector<std::pair<std::vector<int>::iterator, std::vector<int>::iterator>> two_partitions(std::vector<int>::iterator, std::vector<int>::iterator, int);
+std::vector<std::pair<std::vector<int>::iterator, std::vector<int>::iterator>> two_partitions_from_four_items(std::vector<int>::iterator begin, std::vector<int>::iterator end, int number_of_partitions);
 
 class MpiMapReduceTests : public CppUnit::TestFixture
 {
@@ -21,6 +22,9 @@ class MpiMapReduceTests : public CppUnit::TestFixture
     CPPUNIT_TEST(MapCallsPartitioningFunctionWithNumberOfPartitions);
     CPPUNIT_TEST(MapCallsMpiSendOnceForOnePartition);
     CPPUNIT_TEST(MapCallsMpiSendTwiceForTwoPartitions);
+    CPPUNIT_TEST(MapCallsMpiSendWithTheCorrectNumberOfEntriesForThePartitions);
+    CPPUNIT_TEST(MapCallsMpiSendWithTheIntegerRepresentationOfTheStartIteratorForThePartition);
+    CPPUNIT_TEST(MapCallsMpiSendWithTheIntegerRepresentationOfTheEndIteratorForThePartition);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -129,6 +133,52 @@ public:
 		CPPUNIT_ASSERT_EQUAL_MESSAGE("The MPI_Send method was not called the correct number of times based on the given number of partitions, which is not expected.", 2, mpiAdapter.GetNumberOfTimesMpiSendCalled());
 	}
 
+	void MapCallsMpiSendWithTheCorrectNumberOfEntriesForThePartitions()
+	{
+		std::vector<int> input;
+
+		MockMpiAdapater mpiAdapter;
+
+		const int unused_number_of_partitions = 13;
+		auto runner = MpiMapReduce<std::vector<int>::iterator>(mpiAdapter, input.begin(), input.end(), one_partition, unused_number_of_partitions);
+
+		runner.map();
+
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("The MPI_Send method was not called with the correct count of elements based on the given number of partitions, which is not expected.", 2, mpiAdapter.GetCountInMpiSend());
+	}
+
+	void MapCallsMpiSendWithTheIntegerRepresentationOfTheStartIteratorForThePartition()
+	{
+		std::vector<int> input;
+		input.emplace_back(67);
+		input.emplace_back(92);
+
+		MockMpiAdapater mpiAdapter;
+
+		const int unused_number_of_partitions = 13;
+		auto runner = MpiMapReduce<std::vector<int>::iterator>(mpiAdapter, input.begin(), input.end(), one_partition, unused_number_of_partitions);
+
+		runner.map();
+
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("The MPI_Send method was not called the correct first iterator value, which is not expected.", 67, mpiAdapter.GetFirstIteratorValueInMpiSend());
+	}
+
+	void MapCallsMpiSendWithTheIntegerRepresentationOfTheEndIteratorForThePartition()
+	{
+		std::vector<int> input;
+		input.emplace_back(67);
+		input.emplace_back(92);
+
+		MockMpiAdapater mpiAdapter;
+
+		const int unused_number_of_partitions = 13;
+		auto runner = MpiMapReduce<std::vector<int>::iterator>(mpiAdapter, input.begin(), input.end(), one_partition, unused_number_of_partitions);
+
+		runner.map();
+
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("The MPI_Send method was not called the correct end iterator value, which is not expected.", 92, mpiAdapter.GetSecondIteratorValueInMpiSend());
+	}
+
 private:
 	class PartitioningTracker
 	{
@@ -182,29 +232,48 @@ private:
 		virtual void MpiSend(const void* buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm) const
 		{
 			++number_of_times_MPI_Send_called_;
+			count_in_MPI_Send_ = count;
+
+			const int* iterator_values = (const int*)buf;
+
+			first_iterator_value_MPI_Send_ = iterator_values[0];
+			second_iterator_value_MPI_Send_ = iterator_values[1];
 		}
 
-		int GetNumberOfTimesMpiSendCalled() const
-		{
-			return number_of_times_MPI_Send_called_;
-		}
+		int GetNumberOfTimesMpiSendCalled() const { return number_of_times_MPI_Send_called_; }
+		int GetCountInMpiSend() const { return count_in_MPI_Send_; }
+		int GetFirstIteratorValueInMpiSend() const { return first_iterator_value_MPI_Send_; }
+		int GetSecondIteratorValueInMpiSend() const { return second_iterator_value_MPI_Send_; }
 
 	private:
 		mutable int number_of_times_MPI_Send_called_;
+		mutable int count_in_MPI_Send_;
+		mutable int first_iterator_value_MPI_Send_;
+		mutable int second_iterator_value_MPI_Send_;
 	};
 };
 
 std::vector<std::pair<std::vector<int>::iterator, std::vector<int>::iterator>> one_partition(std::vector<int>::iterator begin, std::vector<int>::iterator end, int number_of_partitions)
 {
 	std::vector<std::pair<std::vector<int>::iterator, std::vector<int>::iterator>> output;
-	output.push_back(std::make_pair(begin, end));
+	output.emplace_back(std::make_pair(begin, end));
 	return output;
 }
 
 std::vector<std::pair<std::vector<int>::iterator, std::vector<int>::iterator>> two_partitions(std::vector<int>::iterator begin, std::vector<int>::iterator end, int number_of_partitions)
 {
 	std::vector<std::pair<std::vector<int>::iterator, std::vector<int>::iterator>> output;
-	output.push_back(std::make_pair(begin, end));
-	output.push_back(std::make_pair(begin, end));
+	output.emplace_back(std::make_pair(begin, end));
+	output.emplace_back(std::make_pair(begin, end));
+	return output;
+}
+
+std::vector<std::pair<std::vector<int>::iterator, std::vector<int>::iterator>> two_partitions_from_four_items(std::vector<int>::iterator begin, std::vector<int>::iterator end, int number_of_partitions)
+{
+	std::vector<std::pair<std::vector<int>::iterator, std::vector<int>::iterator>> output;
+	
+	output.emplace_back(std::make_pair(begin, begin+1));
+	output.emplace_back(std::make_pair(begin+2, begin+3));
+
 	return output;
 }
